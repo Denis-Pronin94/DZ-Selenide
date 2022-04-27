aa026e95e33b6668c08dd9dddfbe163cec7b8dca
dddpr <ddd.pronin@yandex.ru>
2022-04-27 19:28:18 +0300

    Added "AnswerOne" and "DragAndDropSelenideTest"

--- src/test/java/gitHub/SelenideTest.java
+++ src/test/java/gitHub/SelenideTest.java
@@ -3,8 +3,6 @@ package gitHub;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -12,7 +10,6 @@ public class SelenideTest {
 
     @Test
     void  openMainPage() {
-
         open("https://github.com");
         $(".header-search-input").click();
         $(".header-search-input").setValue("Selenide").pressEnter();
